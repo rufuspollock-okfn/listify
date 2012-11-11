f15bc659b11b280db4d10656fc62e89e3e51ae44
Rufus Pollock <rufus.pollock@okfn.org>
2012-11-11 11:24:04 +0000

    [refactor][xs]: remove obsolete demoShow method.

--- js/app.js
+++ js/app.js
@@ -22,17 +22,3 @@ var setupGenerator = function() {
   });
 };
 
-var demoShow = function() {
-  var dataset = new recline.Model.Dataset({
-    url: gdocsSourceUrl,
-    backend: 'gdocs'
-  });
-  dataset.fetch().done(function(data) {
-    var template = $('#booktpl').html();
-    var html = Mustache.to_html(template, {
-      resources: dataset.records.toJSON()
-    });
-    $('.js-my-listing').html(html);
-  });
-};
-
